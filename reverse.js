bbd602e2608df128504e1cf0eed9ea15305690e1
kusumareddyvarimadug <kusumareddyvarimadugu@gmail.com>
2022-06-14 21:04:21 +0530

    update changes

---
+++ reverse.js
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>javascript Array reverse Method</title>
+</head>
+<body>
+<script type="text/javascript">
+var arr=[1,2,3].reverse();
+document.write("Reversed array is:"+arr);
+</script>
+</body>
+</html>
